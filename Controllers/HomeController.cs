c811cceaf19b749684c1223709f518db11c58e17
Draefuglestad <draefuglestad@yahoo.com>
2020-11-11 15:49:53 -0500

    changed page formatting

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,20 +16,73 @@ namespace ArafaEmploymentApp.Controllers
         {
             return View();
         }
-        public IActionResult Form()
+        public IActionResult ApplicantInformation()
+        {
+            return View();
+        }
+
+        public IActionResult EmploymentHistory()
         {
             return View();
         }
 
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Form(Applicant applicant)
+        public IActionResult ApplicantInformation(Applicant applicant)
         {
             if (ModelState.IsValid)
             {
-                RedirectToAction("Form");
+                RedirectToAction("EmploymentHistory");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EmploymentHistory(Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                RedirectToAction("EmploymentHistory");
+            }
+            return View();
+        }
+
+        public IActionResult Background()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Background(Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                RedirectToAction("Background");
+            }
+            return View();
+        }
+
+        public IActionResult References()
+        {
+            return View();
+        }
+
+        public IActionResult Submission()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult References(Applicant applicant)
+        {
+            if (ModelState.IsValid)
+            {
+                RedirectToAction("References");
             }
             return View();
         }
