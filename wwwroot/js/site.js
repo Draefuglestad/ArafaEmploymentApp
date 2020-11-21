86904f135d10483368f13a5ceab49dda21630ea6
Alex Derby <n01037933@unf.edu>
2020-11-21 03:29:13 -0500

    Formatting to other pages. Added functions to add employers/references rather than copy/paste

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,10 +2,36 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-$(document).ready(function () {
-
-    $('#sidebarCollapse').on('click', function () {
+function toggleSidebar() {
         $('#sidebar').toggleClass('active');
-    });
+};
 
-});
+var employers = 1;
+var mostRecentlyAddedEmployer = document.getElementById('employer');
+
+function duplicateEmployer() {
+    if (employers < 4) {
+        var clone = mostRecentlyAddedEmployer.cloneNode(true);
+        clone.id = "employer-" + ++employers;
+        mostRecentlyAddedEmployer.insertAdjacentElement("afterend", clone);
+        mostRecentlyAddedEmployer = document.getElementById(`employer-${employers}`);
+    }
+    else {
+        alert("Cannot add another employer. Maximum reached!");
+    }
+}
+
+var references = 1;
+var mostRecentlyAddedReference = document.getElementById('reference');
+
+function duplicateReference() {
+    if (references < 4) {
+        var clone = mostRecentlyAddedReference.cloneNode(true);
+        clone.id = "reference-" + ++references;
+        mostRecentlyAddedReference.insertAdjacentElement("afterend", clone);
+        mostRecentlyAddedReference = document.getElementById(`reference-${references}`);
+    }
+    else {
+        alert("Cannot add another reference. Maximum reached!");
+    }
+}
