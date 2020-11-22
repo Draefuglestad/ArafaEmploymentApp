545685f78937ced879e406b28198ef251ee3cada
Harrison Hess <harrisonhess1@aol.com>
2020-11-21 19:10:31 -0500

    updated background page format and added navbar toggle to all pages

--- Models/Background.cs
+++ Models/Background.cs
@@ -7,29 +7,29 @@ namespace ArafaEmploymentApp.Models
 {
     public class Background
     {
-        [Display(Name = "Did you serve in the military?")]
+        [Display(Name = "Have you ever served in the U.S. military?")]
         public bool MilitaryService { get; set; }
 
-        [Display(Name = "Where you honorably discharged?")]
+        [Display(Name = "Were you honorably discharged?")]
         public bool MilitaryDischarge { get; set; }
 
         [Display(Name = "Years Served")]
         public int YearServed { get; set; }
 
 
-        [Display(Name = "Convicted of a crime?")]
+        [Display(Name = "Have you ever been convicted of a crime?")]
         public bool CrimeBefore { get; set; }
 
-        [Display(Name = "If so, what?")]
+        [Display(Name = "If so, tell us more")]
         public string CrimeType { get; set; }
 
         [Display(Name = "Military Branch (Army, Marines, Navy, Air Force, Coast Guard)")]
         public string MilitaryBranch { get; set; }
 
-        [Display(Name = "Traffic Accidents in the past 3 years?")]
+        [Display(Name = "Number of traffic accidents in the past 3 years")]
         public int TrafficAccident { get; set; }
 
-        [Display(Name = "Traffic Violations in the past 3 years?")]
+        [Display(Name = "Number of traffic violations in the past 3 years")]
         public int TrafficViolation { get; set; }
 
         [Display(Name = "Were you stopped for suspicion of drinking and driving?")]
