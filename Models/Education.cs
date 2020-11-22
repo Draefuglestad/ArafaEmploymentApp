dcea5b36f7adf480f5063882d128f19de60476b3
joe-Ru <ruizjoseph17@gmail.com>
2020-11-22 12:24:56 -0500

    Error handling to education complete

--- Models/Education.cs
+++ Models/Education.cs
@@ -10,20 +10,24 @@ namespace ArafaEmploymentApp.Models
     public class Education
     {
         [Display(Name = "Additional Diplomas, Degrees, etc.")]
+        [Required(ErrorMessage = "Must provide an answer, if non type 'none' ")]
         public string AdditionalEducation { get; set;}
 
         [Display(Name = "Highest Education Earned?")]
-        [Required(ErrorMessage = "Highest Education Required")]
+        [Required(ErrorMessage = "Must provide an answer, if non type 'none' ")]
         public string HighestEducationEarned { get; set;}
 
         [Display(Name = "School Name")]
+        [Required(ErrorMessage = "Must provide an answer, if non type 'none' ")]
         public string SchoolName { get; set; }
 
         [Display(Name = "Graduation Date")]
         [DataType(DataType.Date)]
+        [Required(ErrorMessage = "Must provide an answer, if non type 'none' ")]
         public DateTime? GraduationDate { get; set; }
 
         [Display(Name = "School State")]
+        [Required(ErrorMessage = "Must provide an answer, if non type 'none' ")]
         public string SchoolState { get; set;}
     }
 }
